adeb0861ed88ec745770cb4707cc1487c3b50747
shaycy <shaycy@gmail.com>
2023-02-10 13:50:01 +0200

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,1 +1,4 @@
 sudo docker run -p 8501:8501 --mount type=bind,source="$(pwd)",target=/models/my_model -e MODEL_NAME=my_model -t tensorflow/serving
+
+
+curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://13.95.7.205:8501/v1/models/my_model:predict
