6ab9eb3c933b9e8f0e0de0ca783d462ef2ab6a9c
shaycy <shaycy@gmail.com>
2023-02-19 13:15:57 +0200

    Update serve.sh

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
-sudo docker run -d -p 8501:8501 --mount type=bind,source="$(pwd)",target=/models/my_model -e MODEL_NAME=my_model -t tensorflow/serving
+sudo docker run -d -p 80:8501 --mount type=bind,source="$(pwd)",target=/models/speech2text -e MODEL_NAME=speech2text -t tensorflow/serving
 
 
-curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://13.95.7.205:8501/v1/models/my_model:predict
+curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://13.95.7.205/v1/models/speech2text:predict
